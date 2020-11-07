c90760abcdfdc9d2490a3fdb6a0209e4b32b12a0
larrykooper <larry.kooper@gmail.com>
2020-11-07 16:21:37 -0500

    jquery added and working

--- app/javascript/packs/larry.js
+++ app/javascript/packs/larry.js
@@ -1,1 +1,7 @@
 console.log("I am here in larry.js")
+
+$(document).ready(doThisOnDocumentReady);
+
+function doThisOnDocumentReady() {
+  console.log("DOCUMENT READY");
+}
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+var webpack = require('webpack');
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery'
+  })
+);
+
 module.exports = environment
