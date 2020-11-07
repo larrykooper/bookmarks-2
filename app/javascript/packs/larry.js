6de13a4739fee07d6b4666cffaa6a10ac00ffb86
larrykooper <larry.kooper@gmail.com>
2020-11-07 16:23:08 -0500

    tag sorting working

--- app/javascript/packs/larry.js
+++ app/javascript/packs/larry.js
@@ -4,4 +4,60 @@ $(document).ready(doThisOnDocumentReady);
 
 function doThisOnDocumentReady() {
   console.log("DOCUMENT READY");
+
+  // Sorting the tags in the sidebar when the user asks for it
+  $(".sorter").on("click", sortTagsServerSide);
+}
+
+// SORTING THE TAGS
+
+function sortTagsServerSide(event) {
+  var alphaElem, freqElem, orderElem, orderWanted, otherElem, page, parent;
+  // Figure out which sort order should be bolded
+  alphaElem = $("#alpha");
+  freqElem = $("#freq");
+  orderElem = $(event.target);
+  orderWanted = orderElem.attr("id");
+  orderElem.removeClass("bolder");
+  otherElem = (orderWanted == "alpha" ? freqElem : alphaElem);
+  otherElem.addClass("bolder");
+  // Figure out which page called us
+  parent = orderElem.closest(".tags_sidebar");
+  if (parent.hasClass("bookmarks")) {
+    page = "bookmarks";
+  } else {
+    page = "in_rotation";
+  }
+  // Ajax call
+  $.ajax({
+    url: "/specials/refreshtags",
+    data: {
+      settagsort: orderWanted,
+      page: page
+    },
+    success: putTagsInHtml,
+    error: handleAjaxError
+  });
+}
+
+function putTagsInHtml(data) {
+  var count, id, name, i, oneTag, oneTagHtml;
+  var place = $("#all-the-tags");
+  place.html("")
+  for (i = 0; i < data.length; i++) {
+    count = data[i].count;
+    id = data[i].id;
+    name = data[i].name;
+    var oneTagHtml = `
+      <div class="tag">
+        ${count}
+        <a href="bookmarks/tags='${id}'">${name}</a>
+      </div>
+    `;
+    place.append(oneTagHtml);
+  } // end of for
+}
+
+function handleAjaxError(jqXHR, textStatus, errorThrown) {
+  $('.alert').text(errorThrown);
 }
