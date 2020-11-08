ef723ffabf32a29fe28ea55fd1f8d0914879a763
larrykooper <larry.kooper@gmail.com>
2020-11-08 08:36:49 -0500

    added a comment

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,6 @@
 class TagsController < ApplicationController
 
+  # This is used for autocomplete
   def index
     tag_objs = Tag.where("name LIKE '#{params[:term]}%'")
     @tags = tag_objs.map(&:name)
