867595822924f722a6e0506f62dcc22b1d24ec67
larrykooper <larry.kooper@gmail.com>
2020-11-08 11:23:17 -0500

    Adjusted devise modules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  # Include devise modules.
+  # Defaults are: :database_authenticatable, :registerable,
+  #   :recoverable, :rememberable, :validatable
+  # Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
+  devise :database_authenticatable,
+         :rememberable, :validatable
 
   has_many :user_visits
   has_many :bookmarks
