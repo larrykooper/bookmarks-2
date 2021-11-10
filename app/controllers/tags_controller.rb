051bdda7780db3ca5f422495e8839028d6fbabbd
larrykooper <larry.kooper@gmail.com>
2021-11-10 17:04:56 -0500

    Bug fix: Tag search should be case-insensitive

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,7 +2,7 @@ class TagsController < ApplicationController
 
   # This is used for autocomplete
   def index
-    tag_objs = Tag.where("name LIKE '#{params[:term]}%'")
+    tag_objs = Tag.where("LOWER(name) LIKE LOWER('#{params[:term]}%')")
     @tags = tag_objs.map(&:name)
     render json: @tags
   end
