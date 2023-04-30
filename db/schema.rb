f785c3a9e936179d1531215105df7886a315a294
larrykooper <larry.kooper@gmail.com>
2023-04-30 19:29:28 -0400

    updated pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_11_07_204546) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
