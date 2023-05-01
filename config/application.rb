d5849b7367665492d4008de37289bfc208ca0c19
larrykooper <larry.kooper@gmail.com>
2023-04-30 20:40:31 -0400

    added shakapacker

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Bookmarks2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.autoload_paths << "#{root}/lib/shakapacker"
 
     # Configuration for the application, engines, and railties goes here.
     #
