bab7d3fa310cda8f380ffd3e91bafe54ce2dc673
larrykooper <larry.kooper@gmail.com>
2023-12-29 14:04:13 -0500

    js change

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -14,17 +14,19 @@ require("./larry.js")
 require("./autocomplete.js")
 
 
-require.context('../images', true)
+//require.context('../images', true)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+
+const images = require.context('../images', true)
+
 // const imagePath = (name) => images(name, true)
 
 
-import('../stylesheets/application')
+import('./application.css')
 import('./some')
 import('./autocomp')
 
